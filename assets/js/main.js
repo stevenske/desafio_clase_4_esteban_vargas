25a7d5946d2b82a9f5865c8f5acc1271aed1978f
esteban vargas <esteban.var@outlook.com>
2022-07-05 16:11:52 -0300

    fix indentation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,69 @@
 let price = 0
 let discount = 0
-
-
 let name = prompt("hi!! what's your name?")
+
 alert(`hi ${name} welcome to TrainTrips!!`)
 
 alert("we have 10% discount trip in the next countries:Spain, France, and England")
 
 let trip = prompt("write the country in discount that you want to trip!!").toLowerCase()
 
-function discount10 (a,b){
+function discount10(a, b) {
     return a - (a * b)
 }
 
-while(trip !='accept'){
-    switch (trip){
+
+while (trip != 'accept') {
+    switch (trip) {
         case 'spain':
             price = 1800
-            discount = discount10(price,0.10)
+            discount = discount10(price, 0.10)
             alert(`the price of the trip to ${trip} is $${price} and with the 10% discount the total is ${discount}`)
             trip = prompt("write accept for confirm the trip!!").toLowerCase()
             break
         case 'france':
             price = 2200
-            discount = discount10(price,0.10)
+            discount = discount10(price, 0.10)
             alert(`the price of the trip to ${trip} is $${price} and with the 10% discount the total is ${discount}`)
             trip = prompt("write accept for confirm the trip!!").toLowerCase()
             break
-            case 'england':
-                price = 2100
-                discount = discount10(price,0.10)
-                alert(`the price of the trip to ${trip} is $${price} and with the 10% discount the total is ${discount}`)
-                trip = prompt("write accept for confirm the trip!!").toLowerCase()
-                break
-            case '':
-                trip =prompt('please write a country')
-                break
-            default:
+        case 'england':
+            price = 2100
+            discount = discount10(price, 0.10)
+            alert(`the price of the trip to ${trip} is $${price} and with the 10% discount the total is ${discount}`)
+            trip = prompt("write accept for confirm the trip!!").toLowerCase()
+            break
+        case '':
+            trip = prompt('please write a country')
+            break
+        default:
             alert("we don't have that country in discount")
             trip = prompt("write a country that has a discount and you want to trip!!").toLowerCase()
             break
+    }
+}
+function pay() {
+    let payment = prompt('Do you want to pay it in installments (yes/no)?')
+    if (payment == 'yes') {
+        let installments = prompt('In how many installments do you want to pay it?')
+        let eachInstallment = discount / installments
+        alert(`they will be ${installments} installments of $${eachInstallment} for the total of $${discount} enjoy the trip and thank you for trusting us!!`)
+    } else {
+        let money = prompt('how much will you pay?')
+
+        while (money < discount) {
+            alert('not enough money')
+            money = prompt('how much will you pay?')
         }
+
+        let change = money - discount
+        alert(`your payment is $${money} and his change is $${change} enjoy the trip and thank you for trusting us!!`)
+    }
+
 }
 
-let payment = prompt('Do you want to pay it in installments (yes/no)?')
-if(payment == 'yes'){
-    let installments = prompt('In how many installments do you want to pay it?')
-    let eachInstallment = discount / installments
-    alert(`they will be ${installments} installments of $${eachInstallment} for the total of $${discount} enjoy the trip and thank you for trusting us!!`)
-}else{
-    let money = prompt ('how much will you pay?')
+pay()
 
-while(money < discount){
-    alert('not enough money')
-    money= prompt ('how much will you pay?')
-}
-
-let change = money - discount
-    alert(`your payment is $${money} and his change is $${change} enjoy the trip and thank you for trusting us!!`)
-}
 
 
 
